1fea5aae8a068555a8a96e43a946a87cd4593ad8
Viktoria Tsaneva <viki.caneva07@gmail.com>
2025-12-17 08:29:51 +0200

    Добавен .gitignore и първо качване на проекта

---
+++ calculator 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_1
+{
+
+
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ calculator 1/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_1
+{
+    internal class Test
+    {
+        [Test]
+       public void Add_TwoPlusTwo_ReturnsFour()
+        {
+          
+            var calculator = new Calculator();
+          Assert.That (calculator.Add(2, 2), Is.EqualTo(4));
+        }
+    }
+}
